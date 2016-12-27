cbddfa110004ae8da31a6b354d707ff57c21fe52
Christopher LaChance <christopher.t.lachance@gmail.com>
2016-12-26 19:35:11 -0800

    Added procedural ground generation.
    Converted the ground to a prefab and implemented a parent object to
    generate and contain all the generated ground objects.
    
    TODO: Remove ground objects once the player is out of range.

---
+++ Assets/Scripts/GroundGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundGenerator : MonoBehaviour
+{
+	private GameObject player;
+	private Transform ground;
+	private int groundIndex;
+
+	void Start()
+	{
+		player = GameObject.Find("Player");
+		ground = this.transform.GetChild(0);
+	}
+
+	void Update()
+	{
+		if(player.transform.position.z >= (groundIndex * 1000) - 750)
+		{
+			generateGround();
+		}
+		print(groundIndex);
+	}
+
+	private void generateGround()
+	{
+		groundIndex++;
+		Instantiate(ground, new Vector3(0, 0, groundIndex * 1000), new Quaternion(0, 0, 0, 0), this.transform);
+	}
+}
