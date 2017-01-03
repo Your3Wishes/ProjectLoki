5841e4a8fa432f02b396bb9fd8604fd54ddc31c7
Your3Wishes <guitarfreak993@yahoo.com>
2017-01-02 22:20:30 -0600

    Added Dynamic Launchpad Spawning

--- Assets/Scripts/GroundGenerator.cs
+++ Assets/Scripts/GroundGenerator.cs
@@ -7,6 +7,7 @@ public class GroundGenerator : MonoBehaviour
 	GameObject player;
 	GameObject[] groundPrefabArray;
 	public int groundIndex;
+    public bool UseDynamicLaunchpads;
 
 	void Start()
 	{
@@ -25,8 +26,13 @@ public class GroundGenerator : MonoBehaviour
 	//Advances the ground index and instantiates the next section of ground.
 	private void generateGround()
 	{
-		GameObject randomGround = groundPrefabArray[Random.Range(0, groundPrefabArray.Length)];
-		Instantiate(randomGround, new Vector3(0, 0, (groundIndex * 500) + 250), new Quaternion(0, 0, 0, 0), this.transform);
+        GameObject randomGround;
+        // Using dynamic launchpad spawning only?
+        if (UseDynamicLaunchpads)
+            randomGround = groundPrefabArray[3];
+        else
+            randomGround = groundPrefabArray[Random.Range(0, groundPrefabArray.Length)];
+        Instantiate(randomGround, new Vector3(0, 0, (groundIndex * 500) + 250), new Quaternion(0, 0, 0, 0), this.transform);
 		groundIndex++;
 	}
 }
